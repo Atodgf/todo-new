d8e336092fb6ec962b95623a906e1dc2be924a82
fgdota2 <68871109+fgdota2@users.noreply.github.com>
2020-12-23 19:57:12 +0300

    declare input buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Fcomponent from './Fcomponent';
+import Ccomponent from './Ccomponent';
+
 
 function App() {
+  const todos = [
+    {id:1, firstname: 'sad', lastname:'asd'},
+    {id:2, firstname: 'qwe', lastname:'ewq'}
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Fcomponent todos={todos}/>
+        <Ccomponent />
       </header>
     </div>
   );
---
+++ src/Ccomponent.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+
+export default class Ccomponent extends Component {
+    
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            firstname: '',
+            lastname: '',
+            items: []
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleChange2 = this.handleChange2.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        
+    }
+    
+    handleChange(event) {
+        this.setState({
+            firstname:event.target.value
+        })
+    }
+    handleChange2(event) {
+        this.setState({
+            lastname:event.target.value
+        })
+    }
+    handleSubmit(event) {
+        event.preventDefault()
+        this.setState({
+            items: [...this.state.items, this.state.firstname, this.state.lastname]
+        })
+    }
+
+    render() {
+        return (
+            <div>
+            <ul>
+                    {this.state.items.map((item, index) =>(
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            <form onSubmit={this.handleSubmit}>
+                Firstname:<input value={this.state.firstname} onChange= {this.handleChange}/> 
+                Lastname:<input value={this.state.lastname} onChange= {this.handleChange2}/> 
+                <button type="submit">Submit</button> 
+            </form>
+            </div>
+        )
+    }
+}
